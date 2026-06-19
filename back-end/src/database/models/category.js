33d817a157e05c3db13fff6388cf2c789d01317e
Vidhinavadiya <vidhinavadiya90@gmail.com>
2026-06-19 13:59:31 +0530

    category api set

--- back-end/src/database/models/category.js
+++ back-end/src/database/models/category.js
@@ -25,7 +25,7 @@ module.exports = (sequelize, DataTypes) => {
   }, {
     sequelize,
     modelName: 'Category',
-    tableName: 'categories',
+    tableName: 'Categories',
     timestamps: true
   });
   return Category;
